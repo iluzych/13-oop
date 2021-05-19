2b55c1c39895052940ebf30c42294b2f1c27a916
iluzych <ieva.daugvilaite@gmail.com>
2021-05-19 21:04:35 +0300

    Initial commit

---
+++ js/Dog.js
@@ -0,0 +1,10 @@
+class Dog {
+    constructor(name, fur) {
+        this.name = name;
+        this.furColor = fur;
+        this.legsCount = 4;
+        this.hasTale = true;
+    }
+}
+
+module.exports = Dog;
