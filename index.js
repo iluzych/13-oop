57a993cf1bd9d3adeacbf3ea2e1a0e4c3d24aa01
iluzych <ieva.daugvilaite@gmail.com>
2021-05-20 18:06:35 +0300

    Voice method

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const Dog = require('./js/Dog');
 const rexas = new Dog('Rex', 'brown');
 const spaikas = new Dog('Spike', 'white');
 
-console.log(spaikas.hasTale);
-console.log(rexas.hasTale);
+console.log(spaikas);
+console.log(rexas);
+
+rexas.voice();
+rexas.voice();
+spaikas.voice();
+rexas.voice();
+spaikas.voice();
+spaikas.voice();
--- js/Dog.js
+++ js/Dog.js
@@ -4,6 +4,11 @@ class Dog {
         this.furColor = fur;
         this.legsCount = 4;
         this.hasTale = true;
+        this.voiceCount = 0;
+    }
+
+    voice() {
+        console.log(`${this.name}: au au!! (${++this.voiceCount})`);
     }
 }
 
